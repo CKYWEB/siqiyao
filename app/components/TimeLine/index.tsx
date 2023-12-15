0605f01e25e1eea838aeca6206713a9961e75789
siqi <yao.siq@northeastern.edu>
2023-12-15 16:36:59 -0500

    change: using formatDate in TimeLine

--- app/components/TimeLine/index.tsx
+++ app/components/TimeLine/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import {getExp} from "@/app/utils/experience";
+import {formatDate, getExp} from "@/app/utils/experience";
 import styles from "./timeLine.module.css";
 
 type Props = {
@@ -12,7 +12,7 @@ export default function TimeLine (props: Props) {
             {getExp().map(e=> (
                 <li key={e.id} className="pb-1">
                     <div className="text-gray-500 ps-4">
-                        {new Date(e.startDate).toLocaleString('en-us',{month:'short', year:'numeric'})}
+                        {formatDate(e.startDate)}
                     </div>
                     <div className={`flex items-center space-x-2 border-s-2 border-gray-300 ps-3 pt-2 ${styles.institution}`}>
                         <Image src={e.imagePath} alt="logo" width={30} height={30} />
